77a6dd29ce8bdbaef53fa6d4d435e1b8ae968713
Rangana Pradeep <123355131+PradeepGunawardhana@users.noreply.github.com>
2024-08-02 11:52:59 +0530

    Add files via upload

---
+++ backend/model/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+
+    fname:{type:String,required:true},
+    lname:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true}
+
+})
+
+const User = mongoose.model("Users",userSchema);
+
+module.exports = User;
